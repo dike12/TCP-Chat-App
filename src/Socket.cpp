52b6afe6ed100ae49ed763c80181137fa2350c65
Kabelo Dike <dikeair12@gmail.com>
2025-12-22 23:29:48 -0500

    implemented server side socket methods resolved #5

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -34,3 +34,39 @@ Socket& Socket::operator=(Socket&& other) noexcept {
     }
     return *this;
 }
+
+
+
+void Socket::bind(int port){
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+    int opt = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    
+    if(::bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0){
+        throw std::runtime_error("Bind failed");
+    }
+}
+
+void Socket::listen(int backlog) {
+    if (::listen(sockfd, backlog) < 0) {
+        throw std::runtime_error("Listen failed");
+    }
+}
+
+
+
+Socket Socket::accept() {
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    int client_fd = ::accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+
+    if(client_fd < 0){
+        throw std::runtime_error("Accept failed");
+    }
+
+    return Socket(client_fd);
+}
