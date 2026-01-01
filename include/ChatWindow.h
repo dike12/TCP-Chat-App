219550e531ea1432c2d723a0263ede696601b900
Kabelo Dike <dikeair12@gmail.com>
2025-12-31 20:06:57 -0500

    implemented Gui network manager and the signal wiring

--- include/ChatWindow.h
+++ include/ChatWindow.h
@@ -5,6 +5,10 @@
 #include <QLineEdit>
 #include <QPushButton>
 
+#include "Socket.h"
+#include <atomic>
+#include <thread>
+
 
 class ChatWindow : public QWidget {
     Q_OBJECT
@@ -13,11 +17,24 @@ public:
     ChatWindow(QWidget *parent = nullptr);
     ~ChatWindow();
 
+signals:
+    void msgReceived(QString msg);
+
+private slots:
+    void onSendClicked();
+    void onMessageReceived(QString msg);
+
 private:
     QTextEdit *chatDisplay;
     QLineEdit *inputField;
     QPushButton *sendButton;
+    Socket socket;
 
+    std::thread receiverThread;
+    std::atomic<bool> running;
 
     void setupUI();
+    void connectToServer();
+
+    void receiveLoop();
 };
--- src/ChatWindow.cpp
+++ src/ChatWindow.cpp
@@ -1,13 +1,47 @@
 #include "ChatWindow.h"
 #include <QVBoxLayout>
+#include <iostream>
+#include <QMessageBox>
 
-ChatWindow::ChatWindow(QWidget *parent) : QWidget(parent) {
+
+ChatWindow::ChatWindow(QWidget *parent) : QWidget(parent), running(true) {
     setupUI();
+
+    //connect interna;l signal to UI update slot
+    connect(this, &ChatWindow::msgReceived, this, &ChatWindow::onMessageReceived, Qt::QueuedConnection);
+
+    connect(sendButton, &QPushButton::clicked, this, &ChatWindow::onSendClicked);
+    connect(inputField, &QLineEdit::returnPressed, this, &ChatWindow::onSendClicked);
+    
+    connectToServer();
 }
 
 ChatWindow::~ChatWindow() {
     // Qt will automatically delete child widgets
+
+    running = false;
+    if(receiverThread.joinable()) {
+        receiverThread.detach();
+    }
 }
+
+void ChatWindow::connectToServer() {
+    try
+    {
+        chatDisplay->append("Connecting to server...");
+        socket.connect("127.0.0.1", 8080);
+        chatDisplay->append("Connected!");
+
+        receiverThread = std::thread(&ChatWindow::receiveLoop, this);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        QMessageBox::critical(this, "Connection Error", e.what());
+    }
+    
+}
+
 
 void ChatWindow::setupUI() {
     setWindowTitle("TCP Chat Client");
@@ -33,5 +67,51 @@ void ChatWindow::setupUI() {
     mainLayout->addLayout(bottomLayout);
 
     setLayout(mainLayout);
+}
 
+void ChatWindow::receiveLoop() {
+    char buffer[4096];
+    while(running){
+        try
+        {
+            ssize_t bytes = socket.recv(buffer, 4096);
+
+            if(bytes <= 0){
+                emit msgReceived("System: Disconnected from server.");
+                running = false;
+                break;
+            }
+
+            //emit the signal to the main thread
+            std::string msg(buffer, bytes);
+            emit msgReceived(QString::fromStdString(msg));
+        }
+        catch(...)
+        {
+           running = false;
+           break;
+        }
+    }
+}
+
+void ChatWindow::onMessageReceived(QString message) {
+    chatDisplay->append(message);
+}
+
+void ChatWindow::onSendClicked(){
+    QString text = inputField->text();
+    if(text.isEmpty()) return;
+
+    try{
+        //convert Qt String to std::string and send
+        socket.send(text.toStdString());
+
+        //add own message to chat display
+        chatDisplay->append("Me: " + text);
+
+        //clear input field
+        inputField->clear();
+    } catch(const std::exception& e){
+        chatDisplay->append("Error sending: " + QString(e.what()));
+    }
 }
